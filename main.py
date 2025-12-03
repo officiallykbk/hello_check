000e69feeaf6da53615c204d2610dbb5d577f932
Kwabena Boateng Konadu <officiallykbk@gmail.com>
2025-12-03 17:50:59 +0000

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import socket
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World from Jenkins!", "container_id": socket.gethostname()}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
