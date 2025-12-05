8010af6b69edbeb3d3ed3ab8127ef4696c05bf67
Kwabena Boateng Konadu <officiallykbk@gmail.com>
2025-12-05 19:59:43 +0000

    Added Jenkinsfile for CI/CD

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def read_root():
 
 @app.get("/health")
 def health():
-    return {"message": "At long last we ended with bang...maybe!"}
+    return {"message": "At long last we ended with bang...maybe...maybe!"}
 
 
 if __name__ == "__main__":
